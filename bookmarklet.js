1c5f51938f3fee2f53d7b1a5ceff6403b72f74a3
Allen Ding <71154407+ading2210@users.noreply.github.com>
2022-05-20 01:57:53 +0000

    improved the bookmarklet

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript: var script = document.body.appendChild(document.createElement("script")); script.src="https://edpuzzle.hs.vc/script.js"; script.remove();
+javascript: if (window.location.hostname == "edpuzzle.com") {var script = document.body.appendChild(document.createElement("script")); script.src="https://edpuzzle.hs.vc/script.js"; script.remove();} else {alert("Please run this on https://edpuzzle.com/assignments/[assignment_id]/watch")}
