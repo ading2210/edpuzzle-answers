5910729c94b3d48d1852baf93e827e9d44c8b0c7
ading2210 <ading2019@gmail.com>
2023-05-17 11:53:45 -0700

    fix yt video speed changer on old version

--- app/videospeed.js
+++ app/videospeed.js
@@ -46,25 +46,32 @@ function video_speed() {
     
     if (iframe.id == null) {
       alert("Error: Could not find the Youtube iframe.");
+      return;
+    }
+  
+    let player = opener.YT.get(iframe.id);
+    let events;
+    //search for attribute that stores yt event listeners
+    for (let key in player) {
+      let item = player[key];
+      if (item +"" != "[object Object]") continue;
+      for (let key_2 in item) {
+        let item_2 = item[key_2];
+        
+        if (Array.isArray(item_2) && typeof item_2[1] == "string" && item_2[1].startsWith("on")) {
+          events = item[key_2];
+          break;
+        }
+      }
+      if (events) break;
     }
     
-    else {
-      let player = opener.YT.get(iframe.id);
-      let events;
-      //search for attribute that stores yt event listeners
-      for (let key of Object.keys(player)) {
-        let item = player[key];
-        if (item && Array.isArray(item.h)) {
-          events = item.h;
-        }
+    for (let i=1; i<events.length; i+=3) {
+      let event = events[i];
+      if (event == "onPlaybackRateChange") {
+        //overwrite event listener with a blank function
+        events[i+1] = function(){};
       }
-      for (let i=1; i<events.length; i+=3) {
-        let event = events[i];
-        if (event == "onPlaybackRateChange") {
-          events[i+1] = function(){};
-        }
-      }
-      player.setPlaybackRate(speed);
     }
   }
 
