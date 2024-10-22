121aea432a5c791351c42f1534d3515a86938e36
Exam-Ripper <admin@examripper.com>
2024-10-22 07:36:06 -0700

    fix base url

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if (typeof document.dev_env != "undefined") {
 }
 else {
   //get resources off of github to not inflate the jsdelivr stats
-  base_url = "https://raw.githubusercontent.com/Exam-Ripper/edpuzzleanswers/main";
+  base_url = "https://raw.githubusercontent.com/ading2210/edpuzzle-answers/main";
 }
 
 function http_get(url, callback, headers=[], method="GET", content=null) {
