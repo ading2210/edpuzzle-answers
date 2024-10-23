7e37cb3ee37081b45223bb494742e099a6a8a7ab
Sina Matian <89218912+smatian@users.noreply.github.com>
2024-10-23 09:15:21 -0700

    Redesign menu and refactor the submit all answers (#68)
    * design changes
    
    * change script and the styling
    
    * nvm
    
    * ok this works
    
    * stuff vk6 wanted
    
    * button color
    
    * ok bet
    
    * steal progress bar from examripper source code
    
    * add progress bar
    
    * fix progress bar
    
    * debugging progress bar
    
    * fix progress bar
    
    * ooh
    
    * set steps
    
    * refactor auto answers
    
    * changes
    
    * fix the httpget
    
    * get rid of progress bar
    
    * new script
    
    * get rid of progress bar
    
    * remove that file
    
    * Delete menu.html
    
    * fix base url

--- app/autoanswers.js
+++ app/autoanswers.js
@@ -1,128 +1,150 @@
+
+
 var button = document.getElementById("answers_button");
 
-function httpGet(url, callback, headers=[], method="GET", content=null) {
-  var request = new XMLHttpRequest();
-  request.addEventListener("load", callback);
-  request.open(method, url, true);
-  if (document.edpuzzle_data && document.edpuzzle_data.token) {
-    headers.push(["authorization", document.edpuzzle_data.token]);
+class EdpuzzleAutoanswer {
+  constructor() {
+    this.button = document.getElementById("answers_button");
+    this.csrf = null;
+    this.assignment = document.assignment;
+    this.questions = document.questions;
+
+    // Bind methods to ensure correct 'this' context
+    this.getCSRF = this.getCSRF.bind(this);
+    this.getAttempt = this.getAttempt.bind(this);
+    this.skipVideo = this.skipVideo.bind(this);
+    this.postAnswers = this.postAnswers.bind(this);
+    this.httpGet = this.httpGet.bind(this);
   }
-  for (const header of headers) {
-    request.setRequestHeader(header[0], header[1]);
+
+  init() {
+    this.button.value = "Getting CSRF token...";
+    this.getCSRF();
   }
-  request.send(content);
-}
 
-function init() {
-  button.value = "Getting CSRF token...";
-  getCSRF();
-}
-
-function getCSRF() {
-  var csrfURL = "https://edpuzzle.com/api/v3/csrf";
-  httpGet(csrfURL, function(){
-    var data = JSON.parse(this.responseText);
-    var csrf = data.CSRFToken;
-    button.value = "Getting attempt..."
-    getAttempt(csrf, document.assignment);
-  });
-}
-
-function getAttempt(csrf, assignment) {
-  var id = assignment.teacherAssignments[0]._id;
-  var attemptURL = "https://edpuzzle.com/api/v3/assignments/"+id+"/attempt";
-  httpGet(attemptURL, function(){
-    var data = JSON.parse(this.responseText);
-    button.value = "Skipping video..."
-    skipVideo(csrf, data);
-  });
-}
-
-function skipVideo(csrf, attempt) {
-  var id = attempt._id;
-  var teacher_assignment_id = attempt.teacherAssignmentId;
-  var referrer = "https://edpuzzle.com/assignments/"+teacher_assignment_id+"/watch";;
-  var url2 = "https://edpuzzle.com/api/v4/media_attempts/"+id+"/watch";
-
-  var content = {"timeIntervalNumber": 10};
-  var headers = [
-    ['accept', 'application/json, text/plain, */*'],
-    ['accept_language', 'en-US,en;q=0.9'],
-    ['content-type', 'application/json'],
-    ['x-csrf-token', csrf],
-    ['x-edpuzzle-referrer', referrer],
-    ['x-edpuzzle-web-version', opener.__EDPUZZLE_DATA__.version]
-  ];
-  
-  httpGet(url2, function(){
-    var attemptId = attempt._id;
-    var filteredQuestions = [];
-    
-    for (let i=0; i<document.questions.length; i++) {
-      let question = document.questions[i];
-      if (question.type != "multiple-choice") {continue;}
-      
-      if (filteredQuestions.length == 0) {
-        filteredQuestions.push([question]);
-      }
-      else if (filteredQuestions[filteredQuestions.length-1][0].time == question.time) {
-        filteredQuestions[filteredQuestions.length-1].push(question);
-      }
-      else {
-        filteredQuestions.push([question]);
-      }
-    }
-    
-    if (filteredQuestions.length > 0) {
-      var total = filteredQuestions.length;
-      button.value = "Posting answers...";
-      postAnswers(csrf, document.assignment, filteredQuestions, attemptId, total);
-    }
-  }, headers, "POST", JSON.stringify(content));
-}
-
-function postAnswers(csrf, assignment, remainingQuestions, attemptId, total) {
-  var id = assignment.teacherAssignments[0]._id;
-  var referrer = "https://edpuzzle.com/assignments/"+id+"/watch";
-  var answersURL = "https://edpuzzle.com/api/v3/attempts/"+attemptId+"/answers";
-
-  var content = {answers: []};
-  var now = new Date().toISOString();
-  var questionsPart = remainingQuestions.shift();
-  for (let i=0; i<questionsPart.length; i++) {
-    let question = questionsPart[i];
-    let correctChoices = [];
-    for (let j=0; j<question.choices.length; j++) {
-      let choice = question.choices[j];
-      if (choice.isCorrect) {
-        correctChoices.push(choice._id)
-      }
-    }
-    content.answers.push({
-      "questionId": question._id,
-      "choices": correctChoices,
-      "type": "multiple-choice",
+  getCSRF() {
+    const csrfURL = "https://edpuzzle.com/api/v3/csrf";
+    this.httpGet(csrfURL, (response) => {
+      this.csrf = JSON.parse(response).CSRFToken;
+      this.button.value = "Getting attempt...";
+      this.getAttempt();
     });
   }
-  
-  var headers = [
-    ['accept', 'application/json, text/plain, */*'],
-    ['accept_language', 'en-US,en;q=0.9'],
-    ['content-type', 'application/json'],
-    ['x-csrf-token', csrf],
-    ['x-edpuzzle-referrer', referrer],
-    ['x-edpuzzle-web-version', opener.__EDPUZZLE_DATA__.version]
-  ];
-  httpGet(answersURL, function() {
-    if (remainingQuestions.length == 0) {
-      button.value = "Answers submitted successfully.";
-      opener.location.reload();
+
+  getAttempt() {
+    const id = this.assignment.teacherAssignments[0]._id;
+    const attemptURL = `https://edpuzzle.com/api/v3/assignments/${id}/attempt`;
+    this.httpGet(attemptURL, (response) => {
+      const data = JSON.parse(response);
+      this.button.value = "Skipping video...";
+      this.skipVideo(data);
+    });
+  }
+
+  skipVideo(attempt) {
+    var id = attempt._id;
+    var teacher_assignment_id = attempt.teacherAssignmentId;
+    var referrer = "https://edpuzzle.com/assignments/"+teacher_assignment_id+"/watch";;
+    var url2 = "https://edpuzzle.com/api/v4/media_attempts/"+id+"/watch";
+
+    var content = {"timeIntervalNumber": 10};
+    var headers = [
+      ['accept', 'application/json, text/plain, */*'],
+      ['accept_language', 'en-US,en;q=0.9'],
+      ['content-type', 'application/json'],
+      ['x-csrf-token', this.csrf],
+      ['x-edpuzzle-referrer', referrer],
+      ['x-edpuzzle-web-version', opener.__EDPUZZLE_DATA__.version]
+    ];
+    
+    this.httpGet(url2, () => {
+      var attemptId = attempt._id;
+      var filteredQuestions = [];
+      
+      for (let i=0; i<document.questions.length; i++) {
+        let question = document.questions[i];
+        if (question.type != "multiple-choice") {continue;}
+        
+        if (filteredQuestions.length == 0) {
+          filteredQuestions.push([question]);
+        }
+        else if (filteredQuestions[filteredQuestions.length-1][0].time == question.time) {
+          filteredQuestions[filteredQuestions.length-1].push(question);
+        }
+        else {
+          filteredQuestions.push([question]);
+        }
+      }
+      
+      if (filteredQuestions.length > 0) {
+        var total = filteredQuestions.length;
+        this.button.value = "Posting answers...";
+        // Set total steps for the progress bar
+       
+        this.postAnswers(filteredQuestions, attemptId, total);
+      }
+    }, headers, "POST", JSON.stringify(content));
+  }
+
+  postAnswers(remainingQuestions, attemptId, total) {
+    var id = this.assignment.teacherAssignments[0]._id;
+    var referrer = "https://edpuzzle.com/assignments/"+id+"/watch";
+    var answersURL = "https://edpuzzle.com/api/v3/attempts/"+attemptId+"/answers";
+
+    var content = {answers: []};
+    var questionsPart = remainingQuestions.shift();
+    for (let i=0; i<questionsPart.length; i++) {
+      let question = questionsPart[i];
+      let correctChoices = [];
+      for (let j=0; j<question.choices.length; j++) {
+        let choice = question.choices[j];
+        if (choice.isCorrect) {
+          correctChoices.push(choice._id)
+        }
+      }
+      content.answers.push({
+        "questionId": question._id,
+        "choices": correctChoices,
+        "type": "multiple-choice",
+      });
     }
-    else {
-      button.value = `Posting answers... (${total-remainingQuestions.length+1}/${total})`;
-      postAnswers(csrf, assignment, remainingQuestions, attemptId, total);
+    console.log(remainingQuestions.length)
+    
+    var headers = [
+      ['accept', 'application/json, text/plain, */*'],
+      ['accept_language', 'en-US,en;q=0.9'],
+      ['content-type', 'application/json'],
+      ['x-csrf-token', this.csrf],
+      ['x-edpuzzle-referrer', referrer],
+      ['x-edpuzzle-web-version', opener.__EDPUZZLE_DATA__.version]
+    ];
+    this.httpGet(answersURL, () => {
+      if (remainingQuestions.length == 0) {
+        this.button.value = "Answers submitted successfully.";
+        opener.location.reload();
+      }
+      else {
+        var progress = total - remainingQuestions.length;
+        this.button.value = `Posting answers... (${progress}/${total})`;
+        this.postAnswers(remainingQuestions, attemptId, total);
+      }
+    }, headers, "POST", JSON.stringify(content));
+  }
+
+  httpGet(url, callback, headers = [], method = "GET", content = null) {
+    var request = new XMLHttpRequest();
+    request.addEventListener("load", () => callback(request.responseText));
+    request.open(method, url, true);
+    if (document.edpuzzle_data && document.edpuzzle_data.token) {
+      headers.push(["authorization", document.edpuzzle_data.token]);
     }
-  }, headers, "POST", JSON.stringify(content));
+    for (const header of headers) {
+      request.setRequestHeader(header[0], header[1]);
+    }
+    request.send(content);
+  }
 }
 
-init();
+// Initialize and run the autoanswer process
+const autoanswer = new EdpuzzleAutoanswer();
+autoanswer.init();
